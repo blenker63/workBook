7f6124da24d80265661112e884cd723b10f01810
vaceslavkameronov <slka@mail.ru>
2023-02-19 23:18:28 +0400

    задача - перевернуть массив

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
 public class Main {
-
+    public static void main(String[] args) {
+        char[] arr1 = {'к', 'о', 'т'};
+        char[] arr2 = new char[3];
+        System.out.println(arr1);
+        for (int i = 0; i < arr1.length; i++) {
+            arr2[i] = arr1[arr1.length - 1 - i];
+        }
+        System.out.println(arr2);
+    }
 }
